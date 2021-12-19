d8c86760b870975cbd78cf7325df6719bcd798a9
Dark Shadow <96031279+DarkShadowGithubOfficial@users.noreply.github.com>
2021-12-19 11:49:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,24 +23,8 @@ function toggleSelected(target) {
     selected = false;
   }
 }
-var pieceTypes = ['pawn', 'knight', 'bishop', 'rook', 'queen', 'king'];
 function movePiece(startPos, endPos) {
-  var posDelta = endPos.split('tile')[1] - startPos.split('tile')[1];
   var currentPiece = document.querySelector(`#${startPos}`).children[0];
-  var validMove;
-  for (let i = 0; i < pieceTypes.length; i++) {
-    if (currentPiece.classList.contains(`fa-chess-${pieceTypes[i]}`)) {
-      if (pieceTypes[i] == 'pawn') {
-        var color;
-        if (currentPiece[i].classList.contains('light')) {
-          color = 'light';
-        } else {
-          color = 'dark';
-        }
-        validMove = pawnMoveValid(color, document.querySelector(`#${endPos}`), posDelta);
-      }
-    }
-  }
   if (currentPiece == undefined) {
     console.error("Error. Invalid play detected.");
     alert("Error. Invalid play detected.");
@@ -57,17 +41,4 @@ function movePiece(startPos, endPos) {
   } else if (turn == 0 && currentPiece.classList.contains('dark')) {
     turn = 1;
   }
-}
-function pawnMoveValid(color, destination, delta) {
-  var allowedDelta;
-  if (color == 'light') {
-    allowedDelta = -8;
-  } else if (color == 'dark') {
-    allowedDelta = 8;
-  }
-  var hit = destination.innerHTML != '' ? true : false;
-  if (delta == allowedDelta || hit) {
-    return true;
-  }
-  return false;
 }
